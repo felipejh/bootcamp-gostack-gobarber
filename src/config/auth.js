245c517073318b3d20edc01cd72a2ada2ab337fe
Felipe Hoffmann <felipehoffmann@MacBook-Pro-de-Felipe.local>
2020-02-02 17:28:36 -0300

    Finalização da segunda parte

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+﻿export default {
+  secret: 'f29618255c309de4469993cce24286ea',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 ﻿import { Router } from 'express';
 
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import authMiddleware from './app/middlewares/auth';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message : "Hello"})
-});
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+
+// Middlware global somente para as rotas que estão abaixo desta linha
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
