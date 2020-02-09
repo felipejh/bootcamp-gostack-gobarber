1f1ed786422e27241c6dc290f68bb1965e02b019
Felipe Hoffmann <felipehoffmann@MacBook-Pro-de-Felipe.local>
2020-02-09 17:01:41 -0300

    Término do módulo 02

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: "smtp.mailtrap.io",
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: "65f4008f3f6ae1",
-    pass: "feaab959d876eb"
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
   },
   default: {
     from: 'Equipe GoBarger <noreply@gobarber.com>',
---
+++ src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  // dsn: 'https://849455ce3475464cb587898f98ec66a9@sentry.io/2387228',
+  dsn: process.env.SENTRY_DSN
+}
